afa13561adefbef7604c9de528c664db1ada4842
Hatem <hatem.deux@gmail.com>
2020-02-19 21:57:27 -0500

    note comment added to self

--- express_server.js
+++ express_server.js
@@ -34,3 +34,8 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL:  urlDatabase[req.params.shortURL] /* What goes here? */ };
   res.render("urls_show", templateVars);
 }); 
+/**
+ * NOTE-TO-SELF : the reason "templeteVars" is used in each is the scope, they are not scene bu other "app.get()"s 
+ *                Also, I can send any objects I desire , each object sent to path is treated per page 
+ *                pay attention to the Path/route arg for app.get(arg, whatever) *  
+ * */ 
