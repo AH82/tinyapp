163ff15dc44e4773c81aa0d9a06fe2646e8a40a3
Hatem <hatem.deux@gmail.com>
2020-02-20 14:10:44 -0500

    upcomming tasks added as comments, generateRandomString() implemented, calling it placed in app.post /urls .. (still under construction)

--- express_server.js
+++ express_server.js
@@ -10,8 +10,16 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Function to generate random strings (unique) to create shortURLs IDs
-function generateRandomString() {
-// ADD LOGIC
+function generateRandomString(length) {
+// NOTE : the code has been looked over on the internet, the comments are mine (Hatem) though.
+// length: is how many characters 
+var text = "";
+var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; //all possible/wanted letters to include
+ 
+for (var i = 0; i < length; i++) 
+  text += possible.charAt(Math.floor(Math.random() * possible.length)); // random # between 0-1, multiplied by the number of possible characters -> rounded then character at this position, then pushed into the string.
+ 
+return text;
 }
 
 
@@ -68,5 +76,16 @@ app.get("/urls/:shortURL", (req, res) => {
 // POSTs the form (in urls_new) to /URLs
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
+  generateRandomString(6);               // TASK : call string random generation function for shortURL (logic not implemented yet)
+  // commit the changes in the urlDatabase object.
+
+  // instead of "OK", respond with a redirect - to new page showing the link they created 
+  //- to /urls/:shortURL, where shortURL is the random string we generated.
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
+
+// After we generate our new shortURL, we add it to our database.
+// Our server then responds with a redirect to /urls/:shortURL.
+// Our browser then makes a GET request to /urls/:shortURL.
+// Our server looks up the longURL from the database, sends the shortURL and longURL to the urls_show template, generates the HTML, and then sends this HTML back to the browser.
+// The browser then renders this HTML.
