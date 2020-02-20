14226ef414a293df8806a0f6f8a1af9d31c167d9
Hatem <hatem.deux@gmail.com>
2020-02-19 23:06:32 -0500

    body parser code added; plus a bunch of clarifying comments for my own learning

--- express_server.js
+++ express_server.js
@@ -2,17 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// Code to make the "buffer" in 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// setting "view engine" from Express's default "jade" , to "ejs" (Embedded JavaScript) 
 app.set("view engine", "ejs");
 
+// database of tinyApp URLs
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Homepage
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -30,6 +38,14 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// code to be inserted before the /:id (a.k.a. :shortURL) // order matters!
+// reason: if placed after, Express will think /new is an :id , 
+//          but if this one has precedence (placed before), Express will know to treat it regularely (not ID).
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// this following function is mentioned in the learning modules as :id instead of :shortURL -- I think.
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL:  urlDatabase[req.params.shortURL] /* What goes here? */ };
   res.render("urls_show", templateVars);
