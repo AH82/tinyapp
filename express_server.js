c298a4f8fcb179c53193e156827cef0ab7745319
Hatem <hatem.deux@gmail.com>
2020-02-19 23:55:54 -0500

    more comments added, plus generateRandomString function added but yet to be implemented, + app.post @ /URLSs added + some cosmetics

--- express_server.js
+++ express_server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-// Code to make the "buffer" in 
+/** 
+ * The body-parser library will convert the request body from a Buffer into string that we can read. 
+ * It will then add the data to the req(request) object under the key body.
+ */
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Function to generate random strings (unique) to create shortURLs IDs
+function generateRandomString() {
+// ADD LOGIC
+}
+
 
 // setting "view engine" from Express's default "jade" , to "ejs" (Embedded JavaScript) 
 app.set("view engine", "ejs");
@@ -32,6 +41,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+/**
+ * NOTE-TO-SELF : the reason "templeteVars" is used in each is the scope, they are not scene bu other "app.get()"s 
+ *                Also, I can send any objects I desire , each object sent to path is treated per page 
+ *                pay attention to the Path/route arg for app.get(arg, whatever) *  
+ * */ 
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
@@ -50,8 +64,9 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL:  urlDatabase[req.params.shortURL] /* What goes here? */ };
   res.render("urls_show", templateVars);
 }); 
-/**
- * NOTE-TO-SELF : the reason "templeteVars" is used in each is the scope, they are not scene bu other "app.get()"s 
- *                Also, I can send any objects I desire , each object sent to path is treated per page 
- *                pay attention to the Path/route arg for app.get(arg, whatever) *  
- * */ 
+
+// POSTs the form (in urls_new) to /URLs
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
